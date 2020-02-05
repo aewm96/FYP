df16ca62d20970ffbcd2720546c502d1fecc7c3e
Aidan Wilson <aidan@Aidans-MacBook-Pro.local>
2020-02-05 15:29:14 +0000

    Ambient-Light on Switching on

--- src/Initial_Test.cpp
+++ src/Initial_Test.cpp
@@ -4,22 +4,35 @@ int PinIn1=A0; //LED Light
 int PinIn2=A1; //Ambient Light
 int val1=0;
 int val2=0;
+int AL=0;
+bool flag = false;
 
 void setup() {  
   Serial.begin(9600);
-  pinMode(PinIn1, INPUT); //The output signal goes to pin 5
-  pinMode(PinIn2, INPUT);
+  pinMode(PinIn1, INPUT); //Photodiode 1
+  pinMode(PinIn2, INPUT); //Photodiode 2
+}
+
+void ambientlight (){
+if (flag==false){
+  delay(2000);
+  val1=analogRead(PinIn1);
+  AL=val1;
+  flag=true;
+}
 }
 
 void loop(){
+  ambientlight();
+
   val1 = analogRead(PinIn1);
   val2 = analogRead(PinIn2);
 
-if (val1>=959 && val1 <=970)
+if (val1>=AL-2 && val1 <=AL+8)
 {
   Serial.println("0");
 }
-else if (val1<958)
+else if (val1<AL-2)
 {
   Serial.println("1");
 }
