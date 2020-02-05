3aceae591de58cbcc8a0696c606a3a327eb06bec
Aidan Wilson <aidan@Aidans-MacBook-Pro.local>
2020-02-05 14:50:30 +0000

    Getting Rid Of Op-Amps

--- src/Initial_Test.cpp
+++ src/Initial_Test.cpp
@@ -1,45 +1,36 @@
 #include <Arduino.h>
 
 int PinIn1=A0; //LED Light
-//int PinIn2=A2; //Ambient Light
+int PinIn2=A1; //Ambient Light
 int val1=0;
-//int val2=0;
-//int LEDVAL=0;
+int val2=0;
 
-void setup() {
-   pinMode(PinIn1, INPUT); //The output signal goes to pin 5
-   //pinMode(PinIn2, INPUT);
-   Serial.begin(9600);
+void setup() {  
+  Serial.begin(9600);
+  pinMode(PinIn1, INPUT); //The output signal goes to pin 5
+  pinMode(PinIn2, INPUT);
 }
 
 void loop(){
-  //dsfagafs
   val1 = analogRead(PinIn1);
-  //val2 = analogRead(PinIn2);
-  //LEDVAL=val1-val2;
-  /*if (val==945|| val==946){
-    Serial.print(0);
-    Serial.println();
-    delay(100);
-    }
-    else if (val<=945){
-      Serial.print(1);
-      Serial.println();
-      delay(100);
-      }*/
-  
- 
+  val2 = analogRead(PinIn2);
+
+if (val1>=959 && val1 <=970)
+{
+  Serial.println("0");
+}
+else if (val1<958)
+{
+  Serial.println("1");
+}
+else
+{
+  Serial.println("Error");
+}
   //Serial.print(val1);
-  //Serial.print(" LED Light");
   //Serial.println();
   //Serial.print(val2);
-  //Serial.print(" Ambient Light");
- // Serial.println();
-  Serial.print(val1);
-  //Serial.print(" LED Light Only");
-  //Serial.print("--------------------");
-  Serial.println();
-  Serial.print("WELP");
+  // Serial.println();
   delay(250);
   
  }
