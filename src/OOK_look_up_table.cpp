ed1eac2a3e39b801549afbb106de8ba34563e74e
Aidan Wilson <aidan@Aidans-MacBook-Pro.local>
2020-02-06 21:20:49 +0000

    Look_up_table_simple

---
+++ src/OOK_look_up_table.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+//This file is a simple lookup table of the alphabet and numical values, in other words and ASCII table made simply
+//26+10=36
+//2^36=1296
+/*
+char A=1;
+char B=2;
+char C=3;
+char D=4;
+char E=5;
+char F=6;
+char G=7;
+char H=8;
+char I=9;
+char J=10;
+char K=11;
+char L=12;
+char M=13;
+char N=14;
+char O=15;
+char P=16;
+char Q=17;
+char R=18;
+char S=19;
+char T=20;
+char U=21;
+char V=22;
+char W=23;
+char X=24;
+char Y=25;
+char Z=26;
+*/
+void setup() {}
+
+void loop(){}
+
