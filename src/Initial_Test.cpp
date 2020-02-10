752f9cd8682d4ffce62c88b2b1ea1fd6e00f6c42
Aidan Wilson <aidan@Aidans-MacBook-Pro.local>
2020-02-10 22:38:52 +0000

    Saving some changes

--- src/Initial_Test.cpp
+++ src/Initial_Test.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 
 double PinIn1=A0; //LED Light
-double PinIn2=A1; //Ambient Light
+//double PinIn2=A1; //Ambient Light
 double val1=0;
-double val2=0;
+//double val2=0;
 double AL=0;
 bool flag = false;
 
 void setup() {  
   Serial.begin(9600);
   pinMode(PinIn1, INPUT); //Photodiode 1
-  pinMode(PinIn2, INPUT); //Photodiode 2
+  //pinMode(PinIn2, INPUT); //Photodiode 2
 }
 
 void ambientlight (){
@@ -29,25 +29,25 @@ void loop(){
   ambientlight();
 
   val1 = analogRead(PinIn1);
-  val2 = analogRead(PinIn2);
+  //val2 = analogRead(PinIn2);
 
 if (val1>=AL-2 && val1 <=AL+8)
 {
-  Serial.println("0");
+  //Serial.println("0");
   Serial.println(val1);
-  Serial.println(" ");
+  //Serial.println(" ");
 }
 else if (val1<AL-2)
 {
-  Serial.println("1");
+  //Serial.println("1");
   Serial.println(val1);
-  Serial.println(" ");
+  //Serial.println(" ");
 }
 else
 {
-  Serial.println("Error");
+  //Serial.println("Error");
   Serial.println(val1);
-  Serial.println(" ");
+  //Serial.println(" ");
 }
   /*Serial.print(val1);
   Serial.println();
